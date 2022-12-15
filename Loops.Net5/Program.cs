bcb084dd5128bed05d9922f57e04da83caa4bc3b
Timothy Axford <t.axford2@yahoo.com>
2022-12-14 20:35:15 -0500

    Added example of a Do while loop

--- Loops.Net5/Program.cs
+++ Loops.Net5/Program.cs
@@ -10,11 +10,23 @@ namespace Loops.Net5
         static void Main(string[] args)
         {
             //for loop example
-            for(int counter = 1; counter < 20; counter+=2)
+            /*for(int counter = 1; counter < 20; counter+=2)
             {
                 Console.WriteLine(counter);
             }
-            Console.WriteLine("Forloop is done");
+            Console.WriteLine("Forloop is done");*/
+
+            //Do while loop
+            int counter = 0;
+            do
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }while(counter < 5);
+
+
+
+
             Console.Read();
 
         }
