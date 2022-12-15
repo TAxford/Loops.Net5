7b98bfae995a8bb6e2efa197378f1fe1953711e5
Timothy Axford <t.axford2@yahoo.com>
2022-12-14 20:53:47 -0500

    Added a people counter while loop that increases by 1 every time a user hits enter

--- Loops.Net5/Program.cs
+++ Loops.Net5/Program.cs
@@ -31,10 +31,14 @@ namespace Loops.Net5
 
             //While loop
             int counter = 0;
-            while(counter < 10)
+            string enteredText = "";
+            while(enteredText.Equals(""))
             {
-                Console.WriteLine(counter);
+                Console.WriteLine("Please press enter to increase amount by one and anything else" +  
+                    "+ enter if you want to finish counting");
+                enteredText = Console.ReadLine();
                 counter++;
+                Console.WriteLine("Current people count is {0}", counter);
             }
 
 
